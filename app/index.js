c7ddb2bae873b60357b9c8005b401239d11e7f91
Jeremy Teo <jeremyteo88@hotmail.com>
2021-09-23 23:44:01 +0800

    Finished video ~first component~

--- app/index.js
+++ app/index.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import './index.css'
 
 // Component
 // State
 // Lifecycle
 
-class App extends React Component {
-
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                Hello World!
+            </div>
+        ) 
+    }
 }
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+)
