8c268e0ffab627e75e117f18905ec268ad56dbb9
Jeremy Teo Zheng Cong <jeremyteo88@hotmail.com>
2021-09-25 17:55:44 +0800

    Add in popular.js and add CSS styling for the list menu

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import Popular from './components/Popular'
 
 // Component
 // State
@@ -9,8 +10,8 @@ import './index.css'
 class App extends React.Component {
     render() {
         return (
-            <div>
-                Hello World!
+            <div className="container">
+                <Popular />
             </div>
         ) 
     }
