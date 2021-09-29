f16db79479be2c38e79628d29f6e55a772e3a337
Jeremy Teo <jeremyteo88@hotmail.com>
2021-09-29 23:30:31 +0800

    Added grid boxes to format repos data

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { fetchPopularRepos } from "../utils/api"
+import { FaUser, FaStar, FaCodeBranch, FaExclamationTriangle, FaCode } from 'react-icons/fa'
 
 function LanguageNav ({ selected, onUpdateLanguage }){
     const languages = ['All','JavaScript','Ruby','Java','CSS','Python']
@@ -25,16 +26,49 @@ LanguageNav.propTypes = {
     selected: PropTypes.string.isRequired,
     onUpdateLanguage: PropTypes.func.isRequired,
 }
-
+//<pre>{JSON.stringify(repos, null, 2)}</pre>
 function ReposGrid ({repos}) {
     return(
-        <ul className='grid-space-around'>
-
-            <pre>{JSON.stringify(repos, null, 2)}</pre>
-
+        <ul className='grid space-around'>
             {repos.map((repos,index) => {
                 const { name, owner, html_url, stargazers_count, forks, open_issues } = repos
                 const {login, avatar_url } = owner
+
+                return(
+                    <li key={html_url} className='repo bg-light'>
+                        <h4 className='header-lg center-text'>
+                            #{index + 1}
+                        </h4>
+                        <img
+                            className='avatar'
+                            src={avatar_url}
+                            alt={`Avatar for ${login}`}
+                        />
+                        <h2 className='center-text'>
+                            <a className='link' href={html_url}>{login}</a>
+                        </h2>
+                        <ul className='card-list'>
+                            <li>
+                                <FaUser color='rgb(255, 191, 116' size={22} />
+                                <a href={`https://github.com/${login}`}>
+                                    {login}
+                                </a>
+                            </li>
+                            <li>
+                                <FaStar color='rgb(255, 215, 0)' size={22} />
+                                {stargazers_count.toLocaleString()} stars
+                            </li>
+                            <li>
+                                <FaCodeBranch color='rgb(129, 195, 245)' size={22}/>
+                                {forks.toLocaleString()} forks
+                            </li>
+                            <li>
+                                <FaExclamationTriangle color='rgb(241, 138, 147)' size={22}/>
+                                {open_issues.toLocaleString()} open
+                            </li>
+                        </ul>
+                    </li>
+                )
             })}
             
         </ul>
