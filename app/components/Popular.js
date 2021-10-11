814ab4db8e8fe5bc64da6a4cbded1c8c3a2d5690
Jeremy Teo <jeremyteo88@hotmail.com>
2021-10-11 22:11:04 +0800

    Modified Popular page loading speed

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -146,7 +146,7 @@ function body must wrap the literal in parentheses.
                     selected = {selectedLanguage}
                     onUpdateLanguage = {this.updateLanguage}
                 />
-            {this.isLoading() && <Loading text='Fetching results' speed={50}/>}
+            {this.isLoading() && <Loading text='Fetching results' />}
             {error && <p className='center-text error'>{error}</p>}
             {repos[selectedLanguage] && <ReposGrid repos = {repos[selectedLanguage]} />}
             </React.Fragment>
